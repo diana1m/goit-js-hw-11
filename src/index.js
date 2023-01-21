01ccb44cf25fa45016e4ad4bae00a0ac995e5cd5
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-01-21 17:44:42 +0200

    add infinity scroll

--- src/index.js
+++ src/index.js
@@ -12,51 +12,55 @@ let page = 1;
 let per_page = 50;
 let inputValue ="";
 
-// const options = {
-//     root: null,
-//     rootMargin: '300px',
-//     threshold: 0
-// }
-// let observer = new IntersectionObserver(onLoad, options);
+const options = {
+    root: null,
+    rootMargin: '300px',
+    threshold: 0
+}
+let observer = new IntersectionObserver(onLoad, options);
 
 form.addEventListener("submit", onSubmit);
 
-// function onLoad(entries, observer) {
-//     entries.forEach(entry => {
-//         console.log(entry);
-//         if (entry.isIntersecting) {
-//             page += 1
-//             onSubmit(inputValue)
-//                 .then(data => {
-//                     createMarkup(data.hits)
-//                     if (page * per_page >= data.totalHits) {
-//                         observer.unobserve(guard)
-//                     }
-//                 })
-//                 .catch(err => console.log(err))
-//         }
-//     })
-// }
+function onLoad(entries, observer) {
+    entries.forEach(entry => {
+        // console.log(entry);
+        if (entry.isIntersecting) {
+            page += 1
+            getImages(inputValue)
+                .then(data => {
+                    createMarkup(data.hits);
+                    console.log(data);
+                    console.log (page);
+                    if (page * per_page >= data.totalHits) {
+                        observer.unobserve(guard)
+                    }
+                })
+                .catch(err => console.log(err))
+        }
+    })
+}
 
 function onSubmit(evn){
     evn.preventDefault();
-   inputValue = evn.target[0].value;
-
+    inputValue = evn.target[0].value;
     gallery.innerHTML = "";
-
+    page = 1;
+    
     getImages(inputValue)
     .then(data => {
         if(data.total === 0){
             throw new Error("Sorry, there are no images matching your search query. Please try again.");
         }
-        console.log(data)
+        console.log(data);
         
         createMarkup(data.hits);
-        // observer.observe(guard);
+
+        observer.observe(guard);
         lightbox.refresh();
     })
-    .catch(err => Notiflix.Notify.failure(err.message));
+    .catch(err =>Notiflix.Notify.failure(err.message));
 }
+
 
 async function getImages(inputValue){
     const BASE_URL = "https://pixabay.com/api/";
@@ -64,6 +68,7 @@ async function getImages(inputValue){
     const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`);
     return response.data;
 }
+
 
 function createMarkup(arr){
     const markup = arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads})=>
@@ -90,6 +95,5 @@ function createMarkup(arr){
     `).join('');
 
     gallery.insertAdjacentHTML('beforeend', markup);
-
 }
 
